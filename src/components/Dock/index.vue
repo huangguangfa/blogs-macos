70266cb5a2da85a522929fcf60e1971e27c8530b
hgf <1454556135@qq.com>
2021-10-12 00:42:48 +0800

    [feat]添加trash

--- src/components/Dock/index.vue
+++ src/components/Dock/index.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="tabbars mx-auto fixed bottom-0 right-0 left-0 ">
-        <ul class="tabbars-u justify-between justify-center flex-row flex rounded-none border-gray-400 bg-opacity-20 bg-white blur"
-            @mousemove="tabbarMove" @mouseout="tabbarMouseout">
-            <li 
-                class="tabbar-item duration-150 ease-in flex align-items-center justify-flex-end flex-column relative"
-                v-for="(item,index) in TABABR_NAVIGATIONS"  
-                :key="index">
-                <p class="tabbar-title absolute d-none">{{ item.title }}</p>
-                <img @click="openWindows(index)" class="tabbar-img"  :style="dockStyle(index)"  :data-index="index" :src="item.img">
-            </li>
-        </ul>
+        <div class="flex">
+            <ul class="tabbars-u justify-between justify-center flex-row flex rounded-none border-gray-400 bg-opacity-20 bg-white blur"
+                @mousemove="tabbarMove" @mouseout="tabbarMouseout">
+                <li 
+                    class="tabbar-item duration-150 ease-in flex align-items-center justify-flex-end flex-column relative"
+                    v-for="(item,index) in TABABR_NAVIGATIONS"  
+                    :key="index">
+                    <p class="tabbar-title absolute d-none">{{ item.title }}</p>
+                    <img @click="openWindows(index)" class="tabbar-img"  :style="dockStyle(index)"  :data-index="index" :src="item.img">
+                </li>
+            </ul>
+            <ul class="minimize justify-between justify-center flex-row flex rounded-none border-gray-400 bg-opacity-20 bg-white">
+                <li class="tabbar-item duration-150 ease-in flex align-items-center justify-flex-end flex-column relative" >
+                    <p class="tabbar-title absolute d-none">Trash</p>
+                    <img class="tabbar-img" src="/src/assets/images/tabbar-navigation/trash.png">
+                </li>
+            </ul>
+        </div>
+        
         <app-facetime v-model:show="TABABR_NAVIGATIONS[3].desktop"></app-facetime>
         <app-mpas v-model:show="TABABR_NAVIGATIONS[4].desktop"></app-mpas>
         <app-safari v-model:show="TABABR_NAVIGATIONS[6].desktop"></app-safari>
@@ -88,20 +97,43 @@
             height: 65px;
             box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.17);
             border-top-left-radius: 0.5rem;
-            border-top-right-radius: 0.5rem;
             padding-left: 0.5rem;
             padding-right: 0.5rem;
             box-sizing: border-box;
             border: 1px solid #e5e7eb;
             border-bottom: transparent;
+            border-right: none;
             border-color: rgba(156,163,175,0.3);
             display: flex;
-            .tabbar-item{ 
-                padding-bottom: 10px; 
-                will-change: width height; user-select: none;
-                &:hover .tabbar-title{display: block;}
-                .tabbar-title{ color: black; background-color: rgba(209,213,219,0.8);  padding:5px 10px; border-radius: .375rem; top: -80px;}
-                .tabbar-img{ transition-timing-function: cubic-bezier(0.4, 0, 1, 1); transform-origin: bottom; transition-duration: .15s;  will-change: width height;-webkit-user-drag: none; padding: 0 3px; }
+        }
+        .tabbar-item{ 
+            padding-bottom: 10px; 
+            will-change: width height; user-select: none;
+            &:hover .tabbar-title{display: block;}
+            .tabbar-title{ color: black; background-color: rgba(209,213,219,0.8);  padding:5px 10px; border-radius: .375rem; top: -80px;}
+            .tabbar-img{ transition-timing-function: cubic-bezier(0.4, 0, 1, 1); transform-origin: bottom; transition-duration: .15s;  will-change: width height;-webkit-user-drag: none; padding: 0 3px; }
+        }
+        .minimize{
+            min-width: 70px;
+            height: 65px;
+            border-top-right-radius: 0.5rem;
+            border: 1px solid #e5e7eb;
+            border-color: rgba(156, 163, 175, 0.3);
+            border-left: none;
+            padding-left: 0.5rem;
+            padding-right: 0.5rem;
+            border-bottom: transparent;
+            backdrop-filter: blur(0px);
+            position: relative;
+            img{width: 50px;height: 50px;}
+            &::after{
+                content:"";
+                width: 2px;
+                height: 50px;
+                background: rgba(156,163,175,0.2);
+                position: absolute;
+                top: 7px;left: 10px;
+                border-radius: 10px;
             }
         }
     }
