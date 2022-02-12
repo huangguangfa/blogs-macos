27c10cb2aab3aa9d035d0ecfb2c4ec13cddbca4d
hgf <1454556135@qq.com>
2022-02-13 02:46:37 +0800

    [feat]fcw-ui配置二级域名

--- src/components/apps/fcwUi/index.vue
+++ src/components/apps/fcwUi/index.vue
@@ -2,7 +2,7 @@
     <div class="fcw-ui">
         <window v-model:show="appInfo.desktop" title="fcw-ui库" width="1500" height="800" :appInfo="appInfo">
             <div class="github-content wh100" v-if="appInfo.desktop">
-                <vm-iframe webUrl="http://fcwui.huangguangfa.cn/#/fcw-ui"></vm-iframe>
+                <vm-iframe webUrl="https://huangguangfa.cn/fcwui/"></vm-iframe>
             </div>
         </window>
     </div>
