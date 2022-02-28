8aaec84b44d12f8d036fd9fa4c6cbf80b8614900
gf <1454556135@qq.com>
2022-02-28 16:52:08 +0800

    [feat] sockets 类型

--- src/services/api/sockets.ts
+++ src/services/api/sockets.ts
@@ -3,41 +3,41 @@ import socket from "@/services/socket/index";
 import { socketHost } from "@/config/service.config";
 import { getTemporaryUser } from "@/services/api/user-api";
 import { useSystemStore } from "@/store/index";
-let systemStore:unknown = null
-export async function initScoket(){
-    const { result } = await getTemporaryUser();
-    sessionStorage.setItem('userId',`/ws/${result.userId}`)
+let systemStore: { [Key: string]: any }
+export async function initScoket() {
+    const { result }: { [key: string]: any } = await getTemporaryUser();
+    sessionStorage.setItem('userId', `/ws/${result.userId}`)
     // 初始化临时用户
     let sockets = new socket({
         //网址
-        url:`${socketHost}/${result.userId}`,
+        url: `${socketHost}/${result.userId}`,
         //心跳时间（单位:ms）
-        'heartBeat':5000,
+        'heartBeat': 5000,
         //开起重连
-        'reconnect':true,
+        'reconnect': true,
         //重连间隔时间（单位:ms）
-        'reconnectTime':5000,
+        'reconnectTime': 5000,
         //重连次数
-        'reconnectTimes':10
+        'reconnectTimes': 10
     })
     handleGlobalSocketEvent(sockets);
     return sockets;
 }
 
-function handleGlobalSocketEvent( socket ){
-    socket.onopen( function(){
+function handleGlobalSocketEvent(socket: { [Key: string]: any }) {
+    socket.onopen(function () {
         console.log('%cscoket_success', 'color: green;');
     })
-    socket.onmessage( function( data ){
-        if(!systemStore){
+    socket.onmessage(function (data: string) {
+        if (!systemStore) {
             systemStore = useSystemStore();
         }
         systemStore[SET_GLOABL_SOCKET_DATA](JSON.parse(data))
     })
-    socket.onerror(function (error) {
-        console.log('%cscoket_error', 'color: red;'); 
+    socket.onerror(function () {
+        console.log('%cscoket_error', 'color: red;');
     });
-    socket.onclose(function (data) {
-        console.log('%cscoket_异常关闭', 'color: red;'); 
+    socket.onclose(function () {
+        console.log('%cscoket_异常关闭', 'color: red;');
     })
 }
--- src/services/api/xterm.ts
+++ src/services/api/xterm.ts
@@ -1,5 +1,2 @@
 import { $post } from "@/services/axios";
-import axios from "axios";
-
 export const getTtermId = () => $post('/users/createTemporaryUser');
-export const getTtermIdLoca = () => axios.post('http://localhost:4000/xterm/id');
