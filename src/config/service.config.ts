affaca4abc2258935c76457621260987577f7a5a
hgf <1454556135@qq.com>
2022-03-05 21:44:37 +0800

    [feat]添加类型

--- src/config/service.config.ts
+++ src/config/service.config.ts
@@ -1,7 +1,7 @@
 
 // export const serviceHost = "http://localhost:3078/blogs";
-export const serviceHost = "https://www.huangguangfa.cn/blogs";
+export const serviceHost:string = "https://www.huangguangfa.cn/blogs";
 
 // export const socketHost = "ws://localhost:7788"
 // export const socketHost = "ws://120.25.233.183:7788"
-export const socketHost = "wss://huangguangfa.cn/ws"
+export const socketHost:string = "wss://huangguangfa.cn/ws"
