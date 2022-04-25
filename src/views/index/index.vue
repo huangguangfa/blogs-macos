6815f18db7e131f3b070d6209e091e5420b04e22
hgf <1454556135@qq.com>
2022-04-25 22:05:35 +0800

    feat(s）添加切换tab逻辑

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -5,20 +5,29 @@
 </template>
 
 <script setup>
-    import vmDock from '@/components/Dock/index.vue';
-    import { getCurrentInstance} from "vue";
-    const { proxy } = getCurrentInstance();
-    // 监听键盘事件
-    window.addEventListener('keyup', function(v){
-        proxy.$eventBus.$emit('globalKeyup',{
-            code:v.key
-        })
-    }, true)
+import vmDock from '@/components/Dock/index.vue';
+import { getCurrentInstance } from "vue";
+const { proxy } = getCurrentInstance();
+// 监听键盘事件
+window.addEventListener('keyup', function (v) {
+    proxy.$eventBus.$emit('globalKeyup', {
+        code: v.key
+    })
+}, true)
+window.addEventListener("visibilitychange", () => {
+    if (document.visibilityState == 'hidden') {
+        document.title = '过来玩我呀';
+    } else {
+        document.title = 'Cloud Desktop';
+    }
+})
 </script>
 
 <style lang="less">
-    .home{
-        width: 100%;height: 100vh;overflow: hidden;
-        position: relative;
-    }
+.home {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+}
 </style>
