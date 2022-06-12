965e25158116bdf3e39690b6ccc14e5aa279e8a7
hgf <1454556135@qq.com>
2022-06-12 23:32:50 +0800

    [feat] add types

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,17 +2,22 @@ import vmwindow from "./globalComponents/window/index.vue";
 import vmloading from "./globalComponents/loading/index.vue";
 import vmiframe from "./globalComponents/iframe/index.vue";
 import vmempty from "./globalComponents/empty/index.vue";
-import { Message  } from "./globalComponents/windowTips/Tips";
-export const initGlobalComponent = function (app){
-    let compnentsList = [ 
-        { name:"window", compnent:vmwindow }, 
-        { name:"vmLoading", compnent:vmloading }, 
-        { name:"vmIframe", compnent:vmiframe }, 
-        { name:"vmEmpty", compnent:vmempty }, 
+import { Message } from "./globalComponents/windowTips/Tips";
+import type { App, Component } from "vue"
+type CompnentsList = Array<{
+    name: string,
+    compnent: Component
+}>
+export const initGlobalComponent = function (app: App) {
+    let compnentsList: CompnentsList = [
+        { name: "window", compnent: vmwindow },
+        { name: "vmLoading", compnent: vmloading },
+        { name: "vmIframe", compnent: vmiframe },
+        { name: "vmEmpty", compnent: vmempty },
     ];
-    for( let i=0,len=compnentsList.length; i<len; i++ ){
+    for (let i = 0, len = compnentsList.length; i < len; i++) {
         let { name, compnent } = compnentsList[i];
-        app.component(name,compnent)
+        app.component(name, compnent)
     }
     app.config.globalProperties.$message = Message
 }
