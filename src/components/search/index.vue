f4274eb3c1a0c3710e7de3c683bd5afaf3f83009
hgf <1454556135@qq.com>
2022-07-02 15:36:43 +0800

    feat:添加配置

--- src/components/search/index.vue
+++ src/components/search/index.vue
@@ -1,116 +1,185 @@
 <template>
-    <div class="global-search fixed left-0 top-0">
-        <div class="search-tag absolute ov-hide" v-clickoutside="cancelGlobalSearch">
-            <div class="search-tga-input flex align-items-center">
-                <div class="iconfont macos-sousuo"></div>
-                <input 
-                    class="search-input" 
-                    ref="searchDom"
-                    placeholder="Spotlight Search" 
-                    @input="getInputValue" 
-                    @keyup.enter="enterApps" 
-                    @blur="inputIsFocus = false"
-                    @focus="inputIsFocus = true"
-                    type="text">
-            </div>
-            <div class="search-content">
-                <div class="search-item" 
-                    v-for="(apps, index) in activeApps" 
-                    @click="openApps(apps, index)"
-                    :class="{ 'active': selectIndex === index}"
-                    :key="apps.id">
-                    <img :src="apps.img" alt="">
-                    <span>{{ apps.id }}</span>
-                </div>
-            </div>
+  <div class="global-search fixed left-0 top-0">
+    <div
+      class="search-tag absolute ov-hide"
+      v-clickoutside="cancelGlobalSearch"
+    >
+      <div class="search-tga-input flex align-items-center">
+        <div class="iconfont macos-sousuo"></div>
+        <input
+          class="search-input"
+          ref="searchDom"
+          placeholder="Spotlight Search"
+          @input="getInputValue"
+          @keyup.enter="enterApps"
+          @blur="inputIsFocus = false"
+          @focus="inputIsFocus = true"
+          type="text"
+        />
+      </div>
+      <div class="search-content">
+        <div
+          class="search-item"
+          v-for="(apps, index) in activeApps"
+          @click="openApps(apps, index)"
+          :class="{ active: selectIndex === index }"
+          :key="apps.id"
+        >
+          <img :src="apps.img" alt="" />
+          <span>{{ apps.id }}</span>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-    import { ref, getCurrentInstance, onUnmounted, onMounted } from 'vue';
-    import { SET_START_GLOBAL_SEARCH, SET_TABABR_NAVIGATION, SET_WINDOW_ID  } from "@/config/store.config";
-    import { useSystemStore } from "@/store/index";
-    const systemStore = useSystemStore();
+import { ref, getCurrentInstance, onUnmounted, onMounted } from "vue";
+import {
+  SET_START_GLOBAL_SEARCH,
+  SET_TABABR_NAVIGATION,
+  SET_WINDOW_ID,
+} from "@/config/store.config";
+import { useSystemStore } from "@/store/index";
+const systemStore = useSystemStore();
 
-    const { proxy } = getCurrentInstance();
-    const TABABR_NAVIGATIONS = systemStore.TABABR_NAVIGATION;
-    const activeApps = ref(TABABR_NAVIGATIONS.filter( i => i.id !== 'Trash'));
-    const searchDom = ref(null);
-    let selectIndex = ref(0);
-    let inputIsFocus = ref(false);
-    const keyupName = ["ArrowUp", "ArrowDown"];
+const { proxy } = getCurrentInstance();
+const TABABR_NAVIGATIONS = systemStore.TABABR_NAVIGATION;
+const activeApps = ref(TABABR_NAVIGATIONS.filter((i) => i.id !== "Trash"));
+const searchDom = ref(null);
+let selectIndex = ref(0);
+let inputIsFocus = ref(false);
+const keyupName = ["ArrowUp", "ArrowDown"];
 
-    function globalKeyup(e){
-        const { code } = e;
-        const index = keyupName.indexOf(code);
-        if( index >= 0 ){
-            // 判断键盘上下
-            if( index === 0 && selectIndex.value !== 0 ){
-                selectIndex.value -= 1;
-            }else if( index === 1 && selectIndex.value < activeApps.value.length -1 ){
-                selectIndex.value += 1;
-            }
-            // 选中第一个 且继续按上键、让input得到焦点
-            index === 0 && selectIndex.value === 0 && searchDom.value.focus()
-        }
-        // 不是input且按了enter键盘
-        code === "Enter" && inputIsFocus.value === false && enterApps()
+function globalKeyup(e) {
+  const { code } = e;
+  const index = keyupName.indexOf(code);
+  if (index >= 0) {
+    // 判断键盘上下
+    if (index === 0 && selectIndex.value !== 0) {
+      selectIndex.value -= 1;
+    } else if (index === 1 && selectIndex.value < activeApps.value.length - 1) {
+      selectIndex.value += 1;
     }
-    proxy.$eventBus.$on( "globalKeyup",globalKeyup);
-    // methods
-    function getInputValue(e){
-        const v = e.target.value;
-        const value = TABABR_NAVIGATIONS.filter( i => i.id !== 'Trash' && i.id.toLowerCase().includes(v) );
-        activeApps.value = value;
-        selectIndex.value = 0;
-    }
-    function cancelGlobalSearch(){
-        systemStore[SET_START_GLOBAL_SEARCH](false)
-    }
-    function openApps(apps, index){
-        const appsIndex = TABABR_NAVIGATIONS.findIndex( app => app.id === apps.id);
-        selectIndex.value = index;
-        setTimeout( () =>{
-            systemStore[SET_TABABR_NAVIGATION]({ _index:appsIndex, dockData:{ desktop:true, isMinimize:false } })
-            systemStore[SET_WINDOW_ID](apps.id)
-            cancelGlobalSearch();
-        },100)
-    }
-    function enterApps(){
-        const apps = activeApps.value[selectIndex.value];
-        openApps(apps, selectIndex.value);
-    }
-    onUnmounted( () =>{
-        proxy.$eventBus.$off("globalKeyup", globalKeyup)
-    })
-    onMounted( () =>{
-        selectIndex.value === 0 && searchDom.value.focus()
-    })
+    // 选中第一个 且继续按上键、让input得到焦点
+    index === 0 && selectIndex.value === 0 && searchDom.value.focus();
+  }
+  // 不是input且按了enter键盘
+  code === "Enter" && inputIsFocus.value === false && enterApps();
+}
+proxy.$eventBus.$on("globalKeyup", globalKeyup);
+// methods
+function getInputValue(e) {
+  const v = e.target.value;
+  const value = TABABR_NAVIGATIONS.filter(
+    (i) => i.id !== "Trash" && i.id.toLowerCase().includes(v)
+  );
+  activeApps.value = value;
+  selectIndex.value = 0;
+}
+function cancelGlobalSearch() {
+  systemStore[SET_START_GLOBAL_SEARCH](false);
+}
+function openApps(apps, index) {
+  const appsIndex = TABABR_NAVIGATIONS.findIndex((app) => app.id === apps.id);
+  selectIndex.value = index;
+  setTimeout(() => {
+    systemStore[SET_TABABR_NAVIGATION]({
+      _index: appsIndex,
+      dockData: { desktop: true, isMinimize: false },
+    });
+    systemStore[SET_WINDOW_ID](apps.id);
+    cancelGlobalSearch();
+  }, 100);
+}
+function enterApps() {
+  const apps = activeApps.value[selectIndex.value];
+  openApps(apps, selectIndex.value);
+}
+onUnmounted(() => {
+  proxy.$eventBus.$off("globalKeyup", globalKeyup);
+});
+onMounted(() => {
+  selectIndex.value === 0 && searchDom.value.focus();
+});
 </script>
 
 <style lang="less">
-    .global-search{ width: 100vw;height: 100vh; 
-        .search-tag{width:660px;box-shadow: 0 25px 50px -12px rgba(0,0,0,0.25);backdrop-filter: blur(40px);background-color: rgba(243, 244, 246, 0.8);border: 1px solid rgba(156,163,175,0.5);left: 50%;top: 10%;transform: translate(-50%);border-radius: .375rem;
-            .search-tga-input{width: 100%;height: 59px;padding: 0 10px; box-sizing:border-box;
-                .macos-sousuo{width: 60px;height: 100%;display: flex;align-items: center;justify-content: center;color: rgb(75,85,99);font-size: 30px;}
-                .search-input{width: 100%;height: 100%;border: none;padding: 0;line-height:0;box-sizing: border-box;background: none;
-                    &:focus{border: none;outline:none;}
-                    &::placeholder {
-                        opacity: .4; color: rgba(0,0,0,1);font-size: 28px;
-                        position: relative;
-                        top:5px;
-                    }
-                }
-            }
-            .search-content{width: 100%;max-height: 400px;overflow-y: auto; overflow-x:hidden ; margin-top: 1px;border-top: 1px solid rgba(0,0,0,0.1);padding:10px 10px; box-sizing: border-box;
-                .search-item{
-                    display: flex;align-items: center;margin-top:10px;cursor: pointer;
-                    padding:2px 10px;
-                    img{width: 30px;height: 30px;margin-right: 10px;}
-                }
-                .active{background: #317add;color: #fff;border-radius: 5px;}
-            }
+.global-search {
+  width: 100vw;
+  height: 100vh;
+  .search-tag {
+    width: 660px;
+    box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(40px);
+    background-color: rgba(243, 244, 246, 0.8);
+    border: 1px solid rgba(156, 163, 175, 0.5);
+    left: 50%;
+    top: 10%;
+    transform: translate(-50%);
+    border-radius: 0.375rem;
+    .search-tga-input {
+      width: 100%;
+      height: 59px;
+      padding: 0 10px;
+      box-sizing: border-box;
+      .macos-sousuo {
+        width: 60px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: rgb(75, 85, 99);
+        font-size: 30px;
+      }
+      .search-input {
+        width: 100%;
+        height: 100%;
+        border: none;
+        padding: 0;
+        line-height: 0;
+        box-sizing: border-box;
+        background: none;
+        &:focus {
+          border: none;
+          outline: none;
         }
+        &::placeholder {
+          opacity: 0.4;
+          color: rgba(0, 0, 0, 1);
+          font-size: 28px;
+          position: relative;
+          top: 5px;
+        }
+      }
     }
+    .search-content {
+      width: 100%;
+      max-height: 400px;
+      overflow-y: auto;
+      overflow-x: hidden;
+      margin-top: 1px;
+      border-top: 1px solid rgba(0, 0, 0, 0.1);
+      padding: 10px 10px;
+      box-sizing: border-box;
+      .search-item {
+        display: flex;
+        align-items: center;
+        margin-top: 10px;
+        cursor: pointer;
+        padding: 2px 10px;
+        img {
+          width: 30px;
+          height: 30px;
+          margin-right: 10px;
+        }
+      }
+      .active {
+        background: #317add;
+        color: #fff;
+        border-radius: 5px;
+      }
+    }
+  }
+}
 </style>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,85 +1,225 @@
+import { DATA_REGEX_PATTERN } from "@/config";
 
 //获取随机手机号
 export function getRandomMoble(): string {
-    let prefixArray = new Array("130", "131", "132", "133", "135", "137", "138", "170", "187", "189");
-    let i = Math.floor(10 * Math.random());
-    let prefix = prefixArray[i];
-    for (let j = 0; j < 8; j++) {
-        prefix = prefix + Math.floor(Math.random() * 10);
-    }
-    return prefix;
+  let prefixArray = new Array(
+    "130",
+    "131",
+    "132",
+    "133",
+    "135",
+    "137",
+    "138",
+    "170",
+    "187",
+    "189"
+  );
+  let i = Math.floor(10 * Math.random());
+  let prefix = prefixArray[i];
+  for (let j = 0; j < 8; j++) {
+    prefix = prefix + Math.floor(Math.random() * 10);
+  }
+  return prefix;
 }
 
-
 /*
-*@param Number NameLength 随机获取名字
-*/
+ *@param Number NameLength 随机获取名字
+ */
 export function getRandomName(): string {
-    let index: number = Math.floor(Math.random() * (136 - 0)) + 0;
-    let names: string[] = [
-        "宋江", "卢俊义", "吴用", "公孙胜", "关胜",
-        "林冲", "秦明", "呼延灼", "花荣", "柴进",
-        "李应", "朱仝", "鲁智深", "武松", "董平",
-        "张清", "杨志", "徐宁", "索超", "戴宗",
-        "刘唐", "李逵", "史进", "穆弘", "雷横",
-        "李俊", "阮小二", "张横", "阮小五", "张顺",
-        "阮小七", "杨雄", "石秀", "解珍", "解宝",
-        "燕青", "朱武", "黄信", "孙立", "宣赞",
-        "郝思文", "韩滔", "彭玘", "单廷圭", "魏定国",
-        "萧让", "裴宣", "欧鹏", "邓飞", "燕顺", "杨林",
-        "凌振", "蒋敬", "吕方", "郭盛", "安道全", "皇甫端",
-        "王英", "扈三娘", "鲍旭", "樊瑞", "孔明", "孔亮",
-        "项充", "李衮", "金大坚", "马麟", "童威", "童猛",
-        "孟康", "侯健", "陈达", "杨春", "郑天寿", "陶宗旺",
-        "宋清", "乐和", "龚旺", "丁得孙", "穆春", "曹正",
-        "宋万", "杜迁", "薛永", "李忠", "周通", "汤隆",
-        "杜兴", "邹渊", "邹润", "朱贵", "朱富", "施恩",
-        "蔡福", "蔡庆", "李立", "李云", "焦挺", "石勇",
-        "孙新", "顾大嫂", "张青", "孙二娘", "王定六", "郁保四",
-        "白胜", "时迁", "段景住", "奥特之父", "奥特之母", "奥特之王",
-        "佐菲", "初代", "赛文", "杰克", "艾斯", "泰罗", "雷欧", "阿斯特拉",
-        "爱迪", "尤利安", "葛雷", "帕瓦特", "斯科特", "乔尼亚斯", "迪迦", "盖亚", "戴拿",
-        "奈克瑟斯", "雷杰多", "哉阿斯", "高斯", "麦克斯", "诺亚", "杰斯提斯", "奈欧斯",
-    ]
-    return names[index]
+  let index: number = Math.floor(Math.random() * (136 - 0)) + 0;
+  let names: string[] = [
+    "宋江",
+    "卢俊义",
+    "吴用",
+    "公孙胜",
+    "关胜",
+    "林冲",
+    "秦明",
+    "呼延灼",
+    "花荣",
+    "柴进",
+    "李应",
+    "朱仝",
+    "鲁智深",
+    "武松",
+    "董平",
+    "张清",
+    "杨志",
+    "徐宁",
+    "索超",
+    "戴宗",
+    "刘唐",
+    "李逵",
+    "史进",
+    "穆弘",
+    "雷横",
+    "李俊",
+    "阮小二",
+    "张横",
+    "阮小五",
+    "张顺",
+    "阮小七",
+    "杨雄",
+    "石秀",
+    "解珍",
+    "解宝",
+    "燕青",
+    "朱武",
+    "黄信",
+    "孙立",
+    "宣赞",
+    "郝思文",
+    "韩滔",
+    "彭玘",
+    "单廷圭",
+    "魏定国",
+    "萧让",
+    "裴宣",
+    "欧鹏",
+    "邓飞",
+    "燕顺",
+    "杨林",
+    "凌振",
+    "蒋敬",
+    "吕方",
+    "郭盛",
+    "安道全",
+    "皇甫端",
+    "王英",
+    "扈三娘",
+    "鲍旭",
+    "樊瑞",
+    "孔明",
+    "孔亮",
+    "项充",
+    "李衮",
+    "金大坚",
+    "马麟",
+    "童威",
+    "童猛",
+    "孟康",
+    "侯健",
+    "陈达",
+    "杨春",
+    "郑天寿",
+    "陶宗旺",
+    "宋清",
+    "乐和",
+    "龚旺",
+    "丁得孙",
+    "穆春",
+    "曹正",
+    "宋万",
+    "杜迁",
+    "薛永",
+    "李忠",
+    "周通",
+    "汤隆",
+    "杜兴",
+    "邹渊",
+    "邹润",
+    "朱贵",
+    "朱富",
+    "施恩",
+    "蔡福",
+    "蔡庆",
+    "李立",
+    "李云",
+    "焦挺",
+    "石勇",
+    "孙新",
+    "顾大嫂",
+    "张青",
+    "孙二娘",
+    "王定六",
+    "郁保四",
+    "白胜",
+    "时迁",
+    "段景住",
+    "奥特之父",
+    "奥特之母",
+    "奥特之王",
+    "佐菲",
+    "初代",
+    "赛文",
+    "杰克",
+    "艾斯",
+    "泰罗",
+    "雷欧",
+    "阿斯特拉",
+    "爱迪",
+    "尤利安",
+    "葛雷",
+    "帕瓦特",
+    "斯科特",
+    "乔尼亚斯",
+    "迪迦",
+    "盖亚",
+    "戴拿",
+    "奈克瑟斯",
+    "雷杰多",
+    "哉阿斯",
+    "高斯",
+    "麦克斯",
+    "诺亚",
+    "杰斯提斯",
+    "奈欧斯",
+  ];
+  return names[index];
 }
 
 /*
-*@param Number NameLength 随机获取头像
-*/
+ *@param Number NameLength 随机获取头像
+ */
 export function getRandomuAvatar() {
-    let index = Math.floor(Math.random() * (20 - 0)) + 0;
-    return `https://blogs-macos.oss-cn-shenzhen.aliyuncs.com/generateAvatar/${index}.png`
+  let index = Math.floor(Math.random() * (20 - 0)) + 0;
+  return `https://blogs-macos.oss-cn-shenzhen.aliyuncs.com/generateAvatar/${index}.png`;
 }
 
 /*
-* methods 深拷贝
-*@param source 拷贝对象
-*/
-type _typeObj = { [anyKey: string]: any }
+ * methods 深拷贝
+ *@param source 拷贝对象
+ */
+type _typeObj = { [anyKey: string]: any };
 export function deepClone(source: _typeObj): _typeObj {
-    const targetObj: _typeObj = source.constructor === Array ? [] : {};
-    for (let keys in source) {
-        if (source[keys] && typeof source[keys] === 'object') {
-            targetObj[keys] = deepClone(source[keys]);
-        } else {
-            targetObj[keys] = source[keys];
-        }
+  const targetObj: _typeObj = source.constructor === Array ? [] : {};
+  for (let keys in source) {
+    if (source[keys] && typeof source[keys] === "object") {
+      targetObj[keys] = deepClone(source[keys]);
+    } else {
+      targetObj[keys] = source[keys];
     }
-    return targetObj;
+  }
+  return targetObj;
 }
 
 // 生成随机数
 export const randomNum = function (minNum: number, maxNum: number): number {
-    switch (arguments.length) {
-        case 1:
-            return Math.floor(Math.random() * minNum + 1);
-            break;
-        case 2:
-            return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
-            break;
-        default:
-            return 0;
-            break;
-    }
-} 
+  switch (arguments.length) {
+    case 1:
+      return Math.floor(Math.random() * minNum + 1);
+      break;
+    case 2:
+      return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+      break;
+    default:
+      return 0;
+      break;
+  }
+};
+
+/**
+ * @method 高亮显示文本(常用于关键字搜索)
+ * @param {String} str 需要进行高亮处理的文本
+ * @param {String} keyword 高亮的关键字
+ */
+export const highlight = (str: string, keyword: string, color: string) => {
+  const transform = keyword.replace(DATA_REGEX_PATTERN.highlight, "\\$&");
+  const reg = new RegExp(transform, "gi");
+  if (str) {
+    return str.replace(
+      reg,
+      (text) => `<span style="color:${color}">${text}</span>`
+    );
+  }
+};
