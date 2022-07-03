7d433253bdb6f2d1d60b47b63750fb4ff26eee56
hgf <1454556135@qq.com>
2022-07-03 14:47:27 +0800

    feat:(抽离clock组件

---
+++ src/components/globalComponents/clock/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="clock">
+    <canvas ref="clockRef" height="120" width="120"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+const clockRef = ref();
+let ctx, width, height, r, rem;
+
+onMounted(() => {
+  ctx = clockRef.value.getContext("2d");
+  width = ctx.canvas.width;
+  height = ctx.canvas.height;
+  r = width / 2;
+  rem = width / 230; //比例
+  setInterval(draw, 1000);
+});
+//画圆
+function drawBackground() {
+  ctx.save();
+  ctx.translate(r, r);
+  ctx.beginPath();
+  ctx.lineWidth = 10 * rem;
+  //以0，0为原点，r为半径，0为起始角，2*Math.PI为结束角，顺时针画圆
+  ctx.arc(0, 0, r - ctx.lineWidth / 2, 0, 2 * Math.PI, false);
+  ctx.strokeStyle = "#fff";
+  ctx.stroke();
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+  let hourNumber = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2];
+  ctx.font = 18 * rem + "px Arial";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  //画出1-12的数字
+  ctx.fillStyle = "#000";
+  hourNumber.forEach(function (number, i) {
+    let rad = ((2 * Math.PI) / 12) * i;
+    let x = Math.cos(rad) * (r - 30 * rem);
+    let y = Math.sin(rad) * (r - 30 * rem);
+    ctx.fillText(number, x, y);
+  });
+  ctx.strokeStyle = "#000";
+  //画出秒针走动的60个点
+  // for (let i = 0; i < 60; i++) {
+  //   let rad = ((2 * Math.PI) / 60) * i;
+  //   let x = Math.cos(rad) * (r - 18 * rem);
+  //   let y = Math.sin(rad) * (r - 18 * rem);
+  //   ctx.beginPath();
+  //   if (i % 5 === 0) {
+  //     ctx.fillStyle = "#000";
+  //     ctx.arc(x, y, 2 * rem, 0, 2, 2 * Math.PI, false);
+  //   } else {
+  //     ctx.fillStyle = "#ccc";
+  //     ctx.arc(x, y, 2 * rem, 0, 2, 2 * Math.PI, false);
+  //   }
+  //   ctx.fill();
+  // }
+}
+
+//绘制时针
+function drawHour(hour, minute) {
+  ctx.save();
+  ctx.beginPath();
+  let rad = ((2 * Math.PI) / 12) * hour;
+  let mrad = ((2 * Math.PI) / 12 / 60) * minute;
+  ctx.rotate(rad + mrad);
+  ctx.lineWidth = 6;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 10 * rem);
+  ctx.lineTo(0, -r / 2);
+  ctx.stroke();
+  ctx.restore();
+}
+
+//绘制分针
+function drawMinute(minute) {
+  ctx.save();
+  ctx.beginPath();
+  let rad = ((2 * Math.PI) / 60) * minute;
+  ctx.rotate(rad);
+  ctx.lineWidth = 3 * rem;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 10);
+  ctx.lineTo(0, -r + 30 * rem);
+  ctx.stroke();
+  ctx.restore();
+}
+
+//绘制秒针
+function drawSecond(second) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.fillStyle = "#d89436";
+  let rad = ((2 * Math.PI) / 60) * second;
+  ctx.rotate(rad);
+  ctx.moveTo(-2, 20 * rem);
+  ctx.lineTo(2, 20 * rem);
+  ctx.lineTo(1, -r + 18 * rem);
+  ctx.lineTo(-1, -r + 18 * rem);
+  ctx.fill();
+  ctx.restore();
+}
+
+//画时钟上的中心白色原点
+function drawDot() {
+  ctx.beginPath();
+  ctx.fillStyle = "#fff";
+  ctx.arc(0, 0, 3 * rem, 0, 2 * Math.PI, false);
+  ctx.fill();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, width, height);
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  drawBackground();
+  drawHour(hour, minutes);
+  drawMinute(minutes);
+  drawSecond(seconds);
+  drawDot();
+  ctx.restore();
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,22 +2,24 @@ import vmwindow from "./globalComponents/window/index.vue";
 import vmloading from "./globalComponents/loading/index.vue";
 import vmiframe from "./globalComponents/iframe/index.vue";
 import vmempty from "./globalComponents/empty/index.vue";
+import vmClock from "./globalComponents/clock/index.vue";
 import { Message } from "./globalComponents/windowTips/Tips";
-import type { App, Component } from "vue"
+import type { App, Component } from "vue";
 type CompnentsList = Array<{
-    name: string,
-    compnent: Component
-}>
+  name: string;
+  compnent: Component;
+}>;
 export const initGlobalComponent = function (app: App) {
-    let compnentsList: CompnentsList = [
-        { name: "window", compnent: vmwindow },
-        { name: "vmLoading", compnent: vmloading },
-        { name: "vmIframe", compnent: vmiframe },
-        { name: "vmEmpty", compnent: vmempty },
-    ];
-    for (let i = 0, len = compnentsList.length; i < len; i++) {
-        let { name, compnent } = compnentsList[i];
-        app.component(name, compnent)
-    }
-    app.config.globalProperties.$message = Message
-}
+  let compnentsList: CompnentsList = [
+    { name: "window", compnent: vmwindow },
+    { name: "vmLoading", compnent: vmloading },
+    { name: "vmIframe", compnent: vmiframe },
+    { name: "vmEmpty", compnent: vmempty },
+    { name: "vmClock", compnent: vmClock },
+  ];
+  for (let i = 0, len = compnentsList.length; i < len; i++) {
+    let { name, compnent } = compnentsList[i];
+    app.component(name, compnent);
+  }
+  app.config.globalProperties.$message = Message;
+};
